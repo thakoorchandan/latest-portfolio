d124868be4dd0d0767fe4cc18fa4e02c7ffe4600
Thakoor Chandan <37981807+thakoorchandan@users.noreply.github.com>
2024-05-22 11:37:41 +0530

    Cards Fixed

--- app/components/projectCard.jsx
+++ app/components/projectCard.jsx
@@ -2,22 +2,22 @@
 function ProjectCard(props) {
   const { name, imageUrl, link, techStack, description } = props.projectDetails;
   return (
-    <div className="w-72 h-192 shadow-lg flex flex-col justify-between items-center m-8 p-6 relative group dark:border-2 border-white rounded">
-      <img src={imageUrl} alt="skill" className=""></img>
-      <div class="text-[20px] text-black dark:text-white font-bold py-4">
+    <div className="w-72 h-auto shadow-lg flex flex-col justify-between items-center m-8 p-6 relative group dark:border-2 border-white rounded">
+      <img src={imageUrl} alt="skill" className="w-full h-48 object-cover rounded-md mb-4"></img>
+      <div className="text-[20px] text-black dark:text-white font-bold py-2 text-center">
         {name}
       </div>
-      <div class="text-[14px] text-black dark:text-white font-semibold text-center">
+      <div className="text-[14px] text-black dark:text-white font-semibold text-center mb-2">
         {techStack}
       </div>
-      <div class="text-[12px] text-black dark:text-white py-4">
+      <div className="text-[12px] text-black dark:text-white py-2 text-center">
         {description}
       </div>
       <a
         href={link}
         rel="noopener"
         target="_blank"
-        className="text-black text-[16px] underline font-bold dark:text-white"
+        className="text-black text-[16px] underline font-bold dark:text-white mt-4"
       >
         Link
       </a>
